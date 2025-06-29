af012dc6859cf772425bf5722381a5b3139782c7
Vladimir Gitlevich <vlad@omegaprojects.com>
2025-06-29 12:48:37 -0700

    Initial Commit

---
+++ PapaTubeTests/PapaTubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PapaTubeTests.swift
+//  PapaTubeTests
+//
+//  Created by Vladimir Gitlevich on 6/29/25.
+//
+
+import Testing
+@testable import PapaTube
+
+struct PapaTubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
